b32286d5566f05b553a6b3f0ef6e2b08ff54189a
ttarvis <fakeaccount@mailinator.com>
2013-06-04 19:54:27 -0500

    added test

---
+++ test/test.js
@@ -0,0 +1,11 @@
+var pmModule = require("/home/node-proxmox/lib/proxmox")('user', 'password', 'domain.com');
+var assert = require("assert");
+
+
+pmModule.getClusterStatus(function(err, response){
+  if (err) throw err;
+  else {
+    data = JSON.parse(response);
+    assert(response.data, =!null, 'response null');
+  }
+});
