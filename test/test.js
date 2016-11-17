6d6c5dfd87a118e0cf66e50fa52f2169b1c77fcc
ghostwolf <ghostwolf@ghostwolf>
2016-11-17 12:29:18 +0100

    added some missing functions

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var pmModule = require("../lib/proxmox")('user', 'pass', 'ip');
 var assert = require("assert");
-
+/*
 pmModule.qemu.getStatusCurrent('proxmoxTestbak',100,function(err, response){
   if (err) throw err;
   else {
@@ -8,3 +8,4 @@ pmModule.qemu.getStatusCurrent('proxmoxTestbak',100,function(err, response){
   console.log(data);
   }
 });
+*/
